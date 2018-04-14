b02c3dbe7c03f6f55280af58183d4042a04a746f
szymon <wojtkowski.sz@gmail.com>
2018-04-14 17:41:00 +0200

    moved variables to a separate file, cleaned up terraform configuration structure

--- executor.tf
+++ executor.tf
@@ -1,32 +1,10 @@
-# reading aws region from variables file
+# defining aws region
 
 provider "aws" {
   region = "${var.aws_region}"
 }
 
-# defining aws roles and policies
-
-data "aws_iam_policy_document" "assume-role-lambda" {
-  statement {
-    actions = ["sts:AssumeRole"]
-
-    principals {
-      type = "Service"
-      identifiers = ["lambda.amazonaws.com"]
-    }
-  }
-}
-
-data "aws_iam_policy_document" "assume-role-sfn" {
-  statement {
-    actions = ["sts:AssumeRole"]
-
-    principals {
-      type = "Service"
-      identifiers = ["states.${var.aws_region}.amazonaws.com"]
-    }
-  }
-}
+# defining policies for aws s3 read and write access
 
 data "aws_iam_policy_document" "s3-access" {
     statement {
@@ -40,6 +18,58 @@ data "aws_iam_policy_document" "s3-access" {
     }
 }
 
+resource "aws_iam_policy" "s3-access" {
+    name = "s3-access"
+    path = "/"
+    policy = "${data.aws_iam_policy_document.s3-access.json}"
+}
+
+# defining aws roles and policies for a lambda function
+
+data "aws_iam_policy_document" "lambda-assume-role" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role" "lambda-exec" {
+  name = "lambda-exec-role"
+  assume_role_policy = "${data.aws_iam_policy_document.lambda-assume-role.json}"
+}
+
+resource "aws_iam_role_policy_attachment" "lambda-exec" {
+    role       = "${aws_iam_role.lambda-exec.name}"
+    policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+resource "aws_iam_role_policy_attachment" "s3-access" {
+    role       = "${aws_iam_role.lambda-exec.name}"
+    policy_arn = "${aws_iam_policy.s3-access.arn}"
+}
+
+# defining aws roles and policies for a step functions state machine
+
+data "aws_iam_policy_document" "sfn-assume-role" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type = "Service"
+      identifiers = ["states.${var.aws_region}.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role" "sfn-exec" {
+  name = "sfn-exec"
+  assume_role_policy = "${data.aws_iam_policy_document.sfn-assume-role.json}"
+}
+
 data "aws_iam_policy_document" "lambda-invoke" {
     statement {
         actions = [
@@ -51,79 +81,52 @@ data "aws_iam_policy_document" "lambda-invoke" {
     }
 }
 
-resource "aws_iam_role" "lambda_exec" {
-  name = "serverless_lambda_exec"
-  assume_role_policy = "${data.aws_iam_policy_document.assume-role-lambda.json}"
-}
-
-resource "aws_iam_role_policy_attachment" "basic-exec-role" {
-    role       = "${aws_iam_role.lambda_exec.name}"
-    policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
-}
-
-resource "aws_iam_policy" "s3-access" {
-    name = "s3-access"
-    path = "/"
-    policy = "${data.aws_iam_policy_document.s3-access.json}"
-}
-
-resource "aws_iam_role_policy_attachment" "s3-access" {
-    role       = "${aws_iam_role.lambda_exec.name}"
-    policy_arn = "${aws_iam_policy.s3-access.arn}"
-}
-
-
-resource "aws_iam_role" "state-machine-exec" {
-  name = "state_machine_exec"
-  assume_role_policy = "${data.aws_iam_policy_document.assume-role-sfn.json}"
-}
-
 resource "aws_iam_policy" "lambda-invoke" {
     name = "lambda-invoke"
     policy = "${data.aws_iam_policy_document.lambda-invoke.json}"
 }
 
 resource "aws_iam_role_policy_attachment" "lambda-invoke" {
-    role       = "${aws_iam_role.state-machine-exec.name}"
+    role       = "${aws_iam_role.sfn-exec.name}"
     policy_arn = "${aws_iam_policy.lambda-invoke.arn}"
 }
 
-
 # creating an aws s3 bucket
 
-resource "aws_s3_bucket" "montage-hyperflow-bucket" {
-  bucket = "montage-hyperflow-bucket"
+resource "aws_s3_bucket" "lambda-bucket" {
+  bucket = "${var.bucket_name}"
 }
 
 # adding the lambda archive to the defined bucket
 
-resource "aws_s3_bucket_object" "lambda_package" {
-  bucket = "${aws_s3_bucket.montage-hyperflow-bucket.bucket}"
-  key    = "hyperflowHandler.zip"
+resource "aws_s3_bucket_object" "lambda-package" {
+  bucket = "${aws_s3_bucket.lambda-bucket.bucket}"
+  key    = "${var.lambda_function_bucket_name}"
   source = "${var.lambda_archive_file}"
   etag   = "${md5(file(var.lambda_archive_file))}"
+  depends_on = ["aws_s3_bucket.lambda-bucket"]
 }
 
 # defining aws lambda function
 
-resource "aws_lambda_function" "hyperflow_lambda" {
-  function_name = "HyperflowLambdaHandler3"
+resource "aws_lambda_function" "lambda-function" {
+  function_name = "${var.lambda_function_name}"
 
-  s3_bucket = "${aws_s3_bucket_object.lambda_package.bucket}"
-  s3_key    = "${aws_s3_bucket_object.lambda_package.key}"
+  s3_bucket = "${aws_s3_bucket_object.lambda-package.bucket}"
+  s3_key    = "${aws_s3_bucket_object.lambda-package.key}"
 
-  handler = "HyperflowExecutor.handler"
-  runtime = "nodejs8.10"
-  timeout=15
+  handler = "${var.lambda_handler}"
+  runtime = "${var.lambda_runtime}"
+  timeout= "${var.lambda_timeout}"
 
-  role = "${aws_iam_role.lambda_exec.arn}"
+  role = "${aws_iam_role.lambda-exec.arn}"
 }
 
-# defining aws step functions step machine
+# defining aws step functions state machine
 
 resource "aws_sfn_state_machine" "sfn_state_machine" {
-  name     = "my-state-machine"
-  role_arn = "${aws_iam_role.state-machine-exec.arn}"
+  name     = "${var.step_function_name}"
+  role_arn = "${aws_iam_role.sfn-exec.arn}"
 
   definition = "${data.template_file.sfn-definition.rendered}"
 }
@@ -131,9 +134,9 @@ resource "aws_sfn_state_machine" "sfn_state_machine" {
 # step function definition template
 
 data "template_file" "sfn-definition" {
-  template = "${file("step-function.json")}"
+  template = "${file(var.step_function_definition_file)}"
 
   vars {
-    lambda-arn = "${aws_lambda_function.hyperflow_lambda.arn}"
+    lambda-arn = "${aws_lambda_function.lambda-function.arn}"
   }
 }
--- variables.tf
+++ variables.tf
@@ -2,6 +2,46 @@ variable "aws_region" {
   default = "eu-west-1"
 }
 
+# lambda function variables
+
 variable "lambda_archive_file" {
   default = "lambda.zip"
 }
+
+variable "lambda_function_name" {
+  default = "Executor"
+}
+
+variable "lambda_runtime" {
+  default = "nodejs8.10"
+}
+
+variable "lambda_handler" {
+  default = "Executor.handler"
+}
+
+variable "lambda_timeout" {
+  default = 5
+}
+
+# bucket variables
+
+variable "bucket_name" {
+  default = "executor-bucket"
+}
+
+variable "lambda_function_bucket_name" {
+  default = "lambda.zip"
+}
+
+# state machine variables
+
+variable "step_function_definition_file" {
+  default = "step-function.json"
+}
+
+variable "step_function_name" {
+  default = "StepFunctionWorkflow"
+}
+
+
